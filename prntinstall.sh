aeb85c12b54b0faeb45aa1d23a23d63ce257d4c2
Daniel Moraes <3400263+dankas@users.noreply.github.com>
2019-06-28 14:06:24 -0300

    Mudanças iniciais.
    mudanças iniciais do script.

--- prntinstall.sh
+++ prntinstall.sh
@@ -1,17 +1,7 @@
 #!/bin/bash 
  
 echo "   ********************************************************************* "
-echo "  	                                                                   "
-echo "   _____                                                                 " 
-echo "  /  __ \                                                                "
-echo "  | /  \/  ___   _ __ ___   _ __   _   _   ___   ___   _ __ ___          "
-echo "  | |     / _ \ | '_ ` _ \ | '_ \ | | | | / __| / _ \ | '_ ` _ \         "
-echo "  | \__/\| (_) || | | | | || |_) || |_| || (__ | (_) || | | | | |        " 
-echo "   \____/ \___/ |_| |_| |_|| .__/  \__,_| \___| \___/ |_| |_| |_|        "
-echo "                           | |                                           "
-echo "                           |_|                                           "
-echo "                                                                         "
-echo "  *********************************************************************  "
+echo "   *********************************************************************  "
 echo "   Script de instalação das impressoras do campus Pelotas.               "
 echo "    - O padrão de compartilhamento é o [cpcxxxx]_[modelo da impressora]. "
 echo "    - O compartilhamento pode ser conferido entrando no SGI.             " 
