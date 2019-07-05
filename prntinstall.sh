5feec8385b8e5c681b1a6a4506f1e811cd2e962f
Daniel Moraes <dankasmoraes@gmail.com>
2019-07-05 14:03:53 -0300

    Versão nova do script
    Nova versão com a interface de opções no script.

--- prntinstall.sh
+++ prntinstall.sh
@@ -1,33 +1,53 @@
 #!/bin/bash 
+	clear
 echo "   ********************************************************************* "
 echo "  	                                                                   
-			   ___                                                  
-			  / (_)                                                 
-			 |      __   _  _  _     _          __   __   _  _  _   
-			 |     /  \_/ |/ |/ |  |/ \_|   |  /    /  \_/ |/ |/ |  
-			  \___/\__/   |  |  |_/|__/  \_/|_/\___/\__/   |  |  |_/
-								  /|                                
-								  \|  
+               ___                                                  
+              / (_)                                                 
+             |      __   _  _  _     _          __   __   _  _  _   
+             |     /  \_/ |/ |/ |  |/ \_|   |  /    /  \_/ |/ |/ |  
+              \___/\__/   |  |  |_/|__/  \_/|_/\___/\__/   |  |  |_/
+                                  /|                                
+                                  \|  
 																	       
 																		   "
 echo "   ********************************************************************* "
 echo "   Script de instalação das impressoras do campus Pelotas.               "
 echo "    - O padrão de compartilhamento é o [cpcxxxx]_[modelo da impressora]. "
 echo "    - O compartilhamento pode ser conferido entrando no SGI.             " 
+echo "    - Em caso de erro 'Bad device-uri' instale o smbclient (apt install smbclient)." 
+echo "    - O modelo da impressora está disponivel no CPCLISTA ou pode ser visto
+			  na própria impressora."
 
-	Modelo=7
-	while [Modelo -eq 7 -or modelo -lt 1];
+	INDEX_MODELO_IMPRESSORA=-1
+	CPC_IMPRESSORA="cpcxxxx"
+	MODELO_IMPRESSORA=([1]="MPC300" [2]="SPC431" [3]="MP201" [4]="SP4510SF" [5]="MPCW2200" [6]="MPC401" [7]="MPC2003")
+	echo $INDEX_MODELO_IMPRESSORA
+	echo $CPC_IMPRESSORA
+	echo ${MODELO_IMPRESSORA[1]}
+	while :
 		do
-		echo"Entre com o numero relacionado ao modelo da impressora:
-					1	-MPC 300sr.
-					2	-MPC401.
-					3	-Sp 431.
-					4	-4510.
-					5	-MPc2200w.
-					6	-Mp 201."
-		read Modelo
+			if [[ $INDEX_MODELO_IMPRESSORA -lt 8 && $INDEX_MODELO_IMPRESSORA -gt 0 ]]
+				then
+					break;
+			fi
+			echo " 	Entre com o numero relacionado ao modelo da impressora/multifuncional: "
+			echo "		1 - MPC 300sr."
+			echo "		2 - SP  431."
+			echo "		3 - MP  201."
+			echo "		4 - SP  4510."
+			echo "		5 - MPC 2200w."
+			echo "		6 - MPC 401."
+			echo "		7 - MPC 2003"
+
+			read INDEX_MODELO_IMPRESSORA;
+			#validar()
+
 		done
+echo ${MODELO_IMPRESSORA[$INDEX_MODELO_IMPRESSORA]}
+echo "	Entre com o CPC da impressora/multifuncional:"
+read CPC_IMPRESSORA
+COMPARTILHAMENTO=${CPC_IMPRESSORA}_${MODELO_IMPRESSORA[$INDEX_MODELO_IMPRESSORA]}
+echo $COMPARTILHAMENTO
 
-
-
-	lpadmin -p $1 -E -v smb://sgi/$1 -m drv:///sample.drv/generic.ppd
+	lpadmin -p $COMPARTILHAMENTO -E -v smb://sgi/$COMPARTILHAMENTO -m drv:///sample.drv/generic.ppd
